f3cc1101853345b310facf95cfedc36281ab9b55
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-21 11:40:41 +0530

    Create 5 multiples.c

---
+++ 5 multiples.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+longint sum=0,i,n;
+clrscr();
+printf("\n give the value of N");
+scanf("% 1d",&n);
+for(i=1;i<=n;i++)
+{
+printf("\n 5* % 1d =% 1d",5*i);
+}
+getch();
+return 0;
+}
