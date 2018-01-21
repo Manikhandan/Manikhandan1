27341783961407d89205848e46c206a2441e6e62
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-21 11:01:09 +0530

    Create prime interval.c

---
+++ prime interval.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int low,high,i,f,t,tmp;
+printf("enter the two number");
+scanf("%d%d,&low,&high);
+if(high<low)
+{
+temp=low;
+low=high;
+high=temp;
+}
+printf("%d%d,low,high);
+while(low<high)
+{
+f=0;
+break;
+}
+}
+if(f==0)
+printf("%d",low);
+++low;
+}
+}
