2c7ee7594d56e0c5c1a101ab4e15a19518d3fc37
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-21 11:43:22 +0530

    Create sum of varithmatic.c

---
+++ sum of varithmatic.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,d,n,i,totalnum;
+int sum=0;
+printf("enter the starting number:");
+scanf("%d",&a);
+printf("\nenter the difference of each number:");
+scanf("%d",&d);
+printf("\nenter the total number of terms:");
+scanf("%d",&n);
+sum=n*(2*(a+(n-1)*d))/2;
+totalnum=a+(n-1)*d;
+printf("\n the A.P is:");
+for(i=a;i<=totalnum;i=i+d)
+{
+if(i!=totalnum)
+{
+printf("%d +",i);}
+else
+{
+printf("the result is %d=%d",i,sum);
+}
+}
+return 0;
+}
