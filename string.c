6cbd2ab4f745eefb58c4884db40334d06358f720
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-23 16:39:53 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[10];
+int i,n
+printf("Enter the string");
+scanf("%s",&a);
+printf("Enter tha value of n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%s",a);
+return 0;
+}
+}
