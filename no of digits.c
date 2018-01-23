91bf4200ccc8e5e049fee43123bcc7ec79dc2873
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-23 16:52:14 +0530

    Create no of digits.c

---
+++ no of digits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    long long num;
+    int count = 0;
+    printf("Enter any number: ");
+    scanf("%lld", &num);
+    while(num != 0)
+    {
+        count++;
+        num /= 10;
+    }
+    printf("Total digits: %d", count);
+    return 0;
+}
