e6063f5d0139d6523bb542a0d43ed7300016eab8
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-23 15:58:12 +0530

    Create two times in hr:min.c

---
+++ two times in hr:min.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int h1,m1,h2,m2,h3,m3;
+   scanf("%d%d%d%d",&h1,&m1,&h2,&m2);
+   h3=h1-h2;
+   m3=m1-m2;
+   printf(" %d\t%d",h3,m3);
+   return 0;
+}
