924ab318d7d29e5bc9554238fa1e44d19fdead56
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-23 16:01:29 +0530

    Create count the no charecters.c

---
+++ count the no charecters.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[10];
+int j,b=0;
+scanf("%s",&a[j]);
+while(a[j]!='\0')
+{
+if((a[j]>='a')&&(a[j]<='z'))
+{
+b+=1;
+}
+j++;
+}
+printf("%d",b);
+return 0;
+}
