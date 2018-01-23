158a2546e96c756852ad05357303fcde1d9f5765
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-23 16:49:37 +0530

    Create number n.c

---
+++ number n.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("eneter the number");
+    scanf("%d",&a);
+    if(a>=1&&a<=10)
+    {
+        printf("number is within range");
+    }
+    else
+    {
+        printf("out side range");
+    }
+        
+        
+}
