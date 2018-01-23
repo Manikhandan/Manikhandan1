ce5f7536978333e6a6e017e914744fbf6e107cb4
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-23 16:46:57 +0530

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str1[25],str2[25];
+int i=0,j=0;
+printf("the first string:");
+gets(str1);
+printf(the second string:");
+get(str2);
+while(str[1]!="\0")
+{
+i++;
+while(str[2]!="\0")
+{
+i++;
+j++;
+}
+printf("the enter concadinate string:");
+else
+return 0;
+}
+}
