f687e1c456dfb48ce3ebd9618a470d6d6fb55102
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-25 16:28:06 +0530

    Create power of 2.c

---
+++ power of 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int r;
+scanf("%d",&r);
+if(r%2==0)
+{
+printf("%d is a power of 2",r);
+}
+else
+{
+printf("%d is not a power of 2",r);
+return 0;
+}
+}
