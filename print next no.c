50759b21683d8fea7a95e420a5ac97f27fdbfc0d
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-01-25 16:14:57 +0530

    Create print next no.c

---
+++ print next no.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=123;
+scanf("%d",&a);
+printf("enter the number");
+if(a==123)
+{
+a++;
+printf("enter the next number");
+return 0;
+}
+}
