8a1231c1098d9dc073e714f0653f9e7f55842967
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-02-02 14:05:37 +0530

    Create nearest multiple.c

---
+++ nearest multiple.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+printf("enter the number");
+scanf("%d",&i);
+if(n!=0)
+{
+elseif(n>10)
+{
+printf("the nearest multiples of 10 is 10");
+}
+elseif(n>20)
+{
+printf("the nearest multiples of 10 is 20");
+}
+elseif(n>30)
+{
+printf("the nearest multiples of 10 is 30");
+}
+elseif(n>40)
+{
+printf("the nearest multiples of 10 is 40");
+}
+elseif(n>50)
+{
+printf("the nearest multiples of 10 is 50");
+}
+elseif(n>60)
+{
+printf("the nearest multiples of 10 is 60");
+}
+elseif(n>70)
+{
+printf("the nearest multiples of 10 is 70");
+}
+elseif(n>80)
+{
+printf("the nearest multiples of 10 is 80");
+}
+elseif(n>90)
+{
+printf("the nearest multiples of 10 is 90");
+}
+elseif(n>100)
+{
+printf("the nearest multiples of 10 is 100");
+}
+}
+else
+{
+printf("enter the numbers between 1-99);
+return 0;
+}
+}
