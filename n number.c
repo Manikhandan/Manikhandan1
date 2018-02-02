5464f240635f7028529097ebf125531d40b798f9
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-02-02 14:12:30 +0530

    Create n number.c

---
+++ n number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num%7==0)
+    {
+        printf("\n multiple of 7");
+    }
+    else
+    { printf("\n Not a multiple of 7");
+    }
+    return 0;
+}
