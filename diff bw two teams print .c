3a559e1f4a323ff425328e2b9dc20b18c267567b
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-02-27 16:03:26 +0530

    Create diff bw two teams print .c

---
+++ diff bw two teams print .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main() 
+{	
+    int a,b,i;
+    for(i=0;i<3;i++)
+    {
+        scanf("%d %d",&a,&b);
+        if(n1>n2)
+        {
+            printf("%d\n",a-b);
+        }
+        else
+        {
+            printf("%d\n",b-a);
+        }
+    }
+}
