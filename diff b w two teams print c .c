a912bf03f06b94d4b6997f57b7e9e08d2a4add9a
Manikhandan <31530448+Manikhandan@users.noreply.github.com>
2018-02-27 15:59:11 +0530

    Create diff b w two teams print c .c

---
+++ diff b w two teams print c .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+ int kabali_ninja,opppnent_ninja,i,t;
+ for(i=0;i<3;i++)
+ {
+ scanf("%d %d",&kabali_ninja,&opppnent_ninja);
+  printf("%d",opppnent_ninja-kabali_ninja);
+}
+}
